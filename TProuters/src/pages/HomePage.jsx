0a5603f20ed86b7d9eaf1029a40c95bc994cf23f
LauraSdt <lauramarie.sdtgmail.com>
2024-07-10 14:24:22 +0200

    test api dans homepage

--- TProuters/src/pages/HomePage.jsx
+++ TProuters/src/pages/HomePage.jsx
@@ -1,4 +1,19 @@
+import axios from "axios";
+import { useEffect } from "react";
+
 function HomePage() {
+  useEffect(() => {
+    async function fetchPackages() {
+      const response = await axios.get(
+        "https://registry.npmjs.com/-/v1/search?text=react"
+      );
+
+      console.log(response.data);
+    }
+
+    fetchPackages();
+  }, []);
+
   return (
     <div>
       <h1>Home Page</h1>
